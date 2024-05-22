02ecb6ccd68248c004133d82fabca6788ef0195c
Prathibalakshmi <lakshmibprathiba@gmail.com>
2024-05-22 23:55:57 +0530

    updated the environment

--- setup.py
+++ setup.py
@@ -3,24 +3,28 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-ML-Flow"
+REPO_NAME = "End-to-end-ML-Project-with-MLflow"
 AUTHOR_USER_NAME = "Prathiba Lakshmi B"
 SRC_REPO = "mlproject"
 AUTHOR_EMAIL = "lakshmibprathiba@gmail.com"
+
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for Ml App",
+    description="A small python package for ml app",
     long_description=long_description,
-    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
+
