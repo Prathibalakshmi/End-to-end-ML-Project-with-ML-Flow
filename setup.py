ca8c78554fe27673c4230fef066dbb238b5da009
Prathibalakshmi <lakshmibprathiba@gmail.com>
2024-05-22 13:42:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-end-ML-Project-with-ML-Flow"
+AUTHOR_USER_NAME = "Prathiba Lakshmi B"
+SRC_REPO = "mlproject"
+AUTHOR_EMAIL = "lakshmibprathiba@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for Ml App",
+    long_description=long_description,
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
